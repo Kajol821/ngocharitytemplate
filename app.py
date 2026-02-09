c100b14d119039b1d786da88b369e96e626b13eb
Kajol821 <kajolagarwal2005@gmail.com>
2026-02-09 14:27:33 +0530

    fix the css js img lib issue in inde.html page

--- app.py
+++ app.py
@@ -6,5 +6,34 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/donation')
+def donation():
+    return render_template('donation.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/feature')
+def feature():
+    return render_template('feature.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+@app.route('/testimonial')
+def testimonial():
+    return render_template('testimonial.html')
+
+
+@app.route('/error_404')
+def error_404():
+    return render_template('error_404.html')
+
 if __name__=='__main__':
-    app.run( )
+    app.run(debug=True)
